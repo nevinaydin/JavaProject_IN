3bd9c896c8ac579ca3aee1f4f26f6f2a29429bf6
IdeenNova <ideen@cydeo.com>
2024-03-13 08:25:46 -0400

    Some updates Hello.java

--- src/main/java/com/ideen/Hello.java
+++ src/main/java/com/ideen/Hello.java
@@ -2,6 +2,6 @@ package com.ideen;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World\nGood to be back");
     }
 }
