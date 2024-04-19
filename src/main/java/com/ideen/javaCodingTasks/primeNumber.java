69a7a8020e6ba1dfb77751b87f4e178a760fcea3
IdeenNova <ideen@cydeo.com>
2024-04-19 14:46:14 -0500

    add coding tasks

---
+++ src/main/java/com/ideen/javaCodingTasks/primeNumber.java
@@ -0,0 +1,23 @@
+package com.ideen.javaCodingTasks;
+
+public class primeNumber {
+     /*
+        Write a method that can check if a number is prime or not
+     */
+     public static void main(String[] args) {
+         System.out.println(findPrimeNum(12));
+
+     }
+
+    public static boolean findPrimeNum(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
